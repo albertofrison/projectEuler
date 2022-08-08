1aa48fe573e85393cb52f31d1110a35359d041a6
Alberto Frison <alberto.frison@gmail.com>
2022-08-08 10:21:14 +0200

    Problem 8

--- Script.R
+++ Script.R
@@ -13,10 +13,9 @@ rm(list=ls())
 # Multiples of 3 or 5
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
-list <- c(1:1000)
-sum(list[list %% 3 == 0 | list %% 5 == 0])
-######
-
+list <- c(1:999)
+ans <- sum(list[list %% 3 == 0 | list %% 5 == 0])
+ans # 233168
 
 #####
 # Problem 2
@@ -34,8 +33,8 @@ while (new < 4*10^6) {
   new <- penultimate + last
   fibonacci <- c(fibonacci, new)
 }
-sum (fibonacci[fibonacci %% 2 == 0])
-#####
+ans <- sum (fibonacci[fibonacci %% 2 == 0])
+ans # 4613732
 
 #####
 # Problem 3
@@ -89,7 +88,8 @@ while (!found) {
     found <- TRUE
   }
 }
-primes[i]
+ans <- primes[i]
+ans # 6857
 
 #####
 # Problem 4
@@ -112,7 +112,6 @@ is_palindrome <- function (n) {
 }
 
 # creating a list of products i * j from 100 to 999 each
-
 
 # this process creates a a list with all the i*j results
 list <- array(1000000)
@@ -148,8 +147,8 @@ while (!found) {
   i <- i + 1
   found <- is_palindrome(list[i])
 }
-print (list[i])
-
+ans <- print (list[i]) 
+ans # 906609
 
 ##### 
 # Problem 5
@@ -171,8 +170,7 @@ while (!found) {
     j <- j + 1
   }  
 }
-print (i)
-
+ans <- i # 232792560
 
 ##### 
 # Problem 6
@@ -182,8 +180,8 @@ print (i)
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 l <- c(1:100)
-sum (l)^2 - sum (l^2)
-
+ans <- sum (l)^2 - sum (l^2)
+ans # 25164150
 
 ##### 
 # Problem 7
@@ -225,4 +223,32 @@ while (counter < 10001) {
   i <- i + 2 # next candidate, incremental steps by 2
 }
 
-primes[length(primes)]
+ans <- primes[length(primes)]
+ans # 104743
+
+
+##### 
+# Problem 8
+# Largest Product in a series
+# The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+# Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+
+list <- as.character ("7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450")
+list <- strsplit(list, "")
+
+list <- unlist(lapply(list, strtoi))
+max <- 0
+
+for (i in c(1:(length(list)-13))) {
+  ris <- 1
+  
+  for (j in i : (i+12)) {
+    ris <- ris * list[j]
+  }
+  
+  if (ris > max) {
+    max <- ris
+  }
+}
+ans <- max 
+ans #23514624000
